33cc11847f536113c10df3b5fd0f80ba8c8dca40
yongjing <near711@outlook.com>
2021-01-22 21:07:14 +0800

    JUnit Practice I finished

--- unitTestStarter/src/main/java/com/udacity/examples/Testing/Helper.java
+++ unitTestStarter/src/main/java/com/udacity/examples/Testing/Helper.java
@@ -12,12 +12,12 @@ public class Helper {
 	    * @return
 	    */
 		public static long getCount(List<String> strings){
-			return strings.stream().filter(string->string.isEmpty()).count();
+			return strings.stream().filter(string->!string.isEmpty()).count();
 		}
 		
 		/**
 		 * Method to get the stats of a list of numbers 
-		 * @param numbers
+		 * @param IntSummaryStatistics
 		 * @return
 		 */
 		public static IntSummaryStatistics getStats(List<Integer> expYears) {
--- unitTestStarter/src/test/java/com/udacity/examples/Testing/HelperTest.java
+++ unitTestStarter/src/test/java/com/udacity/examples/Testing/HelperTest.java
@@ -2,10 +2,32 @@ package com.udacity.examples.Testing;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
 public class HelperTest {
 
     @Test
-    public void test () {
+    public void testGetCount () {
+        List<String> emp = new ArrayList<>();
+        emp.add("test1");
+        emp.add("test2");
+        assertEquals(2, Helper.getCount(emp));
+    }
 
+    @Test
+    public void testGetStatus () {
+        List<Integer> emp = Arrays.asList(13,4,15,6,17,8,19,1,2,3);
+        assertEquals(19, Helper.getStats(emp).getMax());
+    }
+
+    @Test
+    public void testGetMergedList () {
+        List<String> emp = Arrays.asList("test1", "", "test2", "");
+        String expected = "test1, test2";
+        assertEquals(expected, Helper.getMergedList(emp));
     }
 }
