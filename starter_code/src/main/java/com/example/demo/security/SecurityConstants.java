d8e57f7cfc8bec62f48721d4ae51e2ed61aee747
yongjing <near711@outlook.com>
2021-01-22 12:32:00 +0800

    security package and files added

---
+++ starter_code/src/main/java/com/example/demo/security/SecurityConstants.java
@@ -0,0 +1,13 @@
+package com.example.demo.security;
+
+/**
+ * contains literal constants that are used in the JWTAuthenticationFilter class
+ */
+public class SecurityConstants {
+
+    public static final String SECRET = "oursecretkey";
+    public static final long EXPIRATION_TIME = 864_000_000;
+    public static final String TOKEN_PREFIX = "Bearer";
+    public static final String HEADER_STRING = "Authorization";
+    public static final String SIGN_UP_URL = "/api/user/create";
+}
