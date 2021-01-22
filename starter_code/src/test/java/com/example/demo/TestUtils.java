fb60f718553e7a44703ec600bf348b1689d41386
yongjing <near711@outlook.com>
2021-01-22 23:11:16 +0800

    TestUtils added

---
+++ starter_code/src/test/java/com/example/demo/TestUtils.java
@@ -0,0 +1,27 @@
+package com.example.demo;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
+public class TestUtils {
+
+    public static void injectObjects (Object target, String fieldName, Object toInject) {
+
+        boolean wasPrivate = false;
+        try {
+            Field field = target.getClass().getDeclaredField(fieldName);
+            boolean accessibility = field.canAccess(target);
+            // check whether the field is private or not
+            if (! accessibility) {
+                field.setAccessible(true);
+                wasPrivate = true;
+            }
+            field.set(target, toInject);
+            if (wasPrivate) {
+                field.setAccessible(false);
+            }
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            e.printStackTrace();
+        }
+    }
+}
